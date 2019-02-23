c9d33c8358274e67fb813bc85dde7a701bad1a4b
oliveirandre <oliveir.andre@ua.pt>
2019-02-23 15:29:11 +0000

    get quote of the day. now we need to process the json response

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,33 +1,53 @@
 package hello;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import net.sf.json.*;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
 	@RequestMapping("/")
-	public String home() {
-		return "Boas Dinis!";
+	public String home() throws IOException {
+                JSONObject json = new JSONObject();
+                
+		return GETQuoteRequest();
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
         
-        public static void GETImageRequest() throws IOException {
-            URL url = new URL("https://source.unsplash.com/random");
+        public static String GETQuoteRequest() throws IOException {
+            URL url = new URL("http://quotes.rest/qod.json");
             String readLine = null;
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod("GET");
-            connection.setRequestProperty("userID", readLine);
+            int responseCode = connection.getResponseCode();
+            
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(
+                   new InputStreamReader(connection.getInputStream()));
+                StringBuffer response = new StringBuffer();
+                while((readLine = in.readLine()) != null) {
+                    response.append(readLine);
+                }
+                in.close();
+                JSONObject json = new JSONObject();
+                
+                return response.toString();
+            }
+            else {
+                return "Error.";
+            }
         }
-
+        
 }
